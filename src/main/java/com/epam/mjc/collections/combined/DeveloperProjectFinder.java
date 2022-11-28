a72dae82ad4f96ecf61741933eee7a2a2e2dad1c
hetot <shaxzod.murtazayev@mail.ru>
2022-11-28 03:39:39 +0300

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                result.add(entry.getKey());
+            }
+        }
+        Comparator<String> comparator = (var1, var2) -> (var1.length() > var2.length() ? -1
+                : var1.length() < var2.length() ? 1
+                : (-1) * var1.compareTo(var2));
+        result.sort(comparator);
+        return result;
     }
 }
